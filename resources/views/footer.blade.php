14ffb8774d92c329a630e6fa25c848ef5e32e383
Chrisblurp <adeyemi504@gmail.com>
2022-12-11 20:54:56 +0100

    updated footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -16,62 +16,70 @@
 </head>
 <body>
     <!-- The footer section -->
-    <div class=" container-fluid my-5">
-    <footer class="text-center text-lg-start">
-    <div class="container p-4">
-        <div class="row my-5">
-        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
-        <div class="d-flex  mb-4 ">
-    <img src="{{URL('images/logo-icon-sm.png')}}">
-</div>
-    
-  
-        
+<footer class="">
+      <div class="container-fluid">
+        <div class=" footer-container row mb-5 ">
+          <div class="col-sm-12 col-md">
+            <div class=" mb-4">
+            <img src="{{URL('images/logo-icon-sm.png')}}">
+            </div>
+          </div>
+          <div class="col-sm-12 col-md">
+            <div class=" install mb-4 ml-md-4">
             
-            <ul list-unstyled d-flex flex-row justify-content-center>
-                <li> <a href="">Install</a> </li>
+              <ul class="list-unstyled">
+              <li> <a href="">Install</a> </li>
                 <li><a href="">Documentation</a></li>
                 <li><a href="">Learn</a></li>
                 <li><a href="">Roadmap</a></li>
-            </ul>
-
-           
-        
-        
-            <ul list-unstyled d-flex flex-row justify-content-center>
-                <li> <a href="">About Us</a> </li>
+              </ul>
+            </div>
+          </div>
+          <div class=" about-us col-sm-12 col-md">
+            <div class=" mb-4 ml-md-4">
+              <ul class="list-unstyled">
+			  <li> <a href="">About Us</a> </li>
                 <li><a href="">Community</a></li>
                 <li><a href="">User surveys</a></li>
                 <li><a href="">contribute</a></li>
                 <li><a href="">Code of conduct</a></li>
-            </ul>
-        
-            <ul class="">
-                <li> <a href="">Get help</a></li>
+              </ul>
+            </div>
+          </div>
+          <div class="get-help col-sm-12 col-md">
+             <div class="mb-4">
+              <ul class="list-unstyled">
+              <li> <a href="">Get help</a></li>
                 <li><a href="">Terms of use</a></li>
                 <li><a href=""> Privacy</a></li>
                 <li><a href="">Press Kit</a></li>
                 <li><a href="">User surveys</a></li>
-            </ul>
-       
-
+              </ul>
+            </div>
+          </div>
+          <div class=" signup col-sm-12 col-md">
+            <div class=" mb-4">
             <p> Sign up for Latest Auth_wiki news, resources and more </p>
             <div class="form-outline form-white mb-4">
-            <input type="text" id="formControlLg" placeholder="Press enter"class="form-control form-control-lg" />
-
+            <input type="text" id="formControlLg" placeholder="&#xf0e0;             Press enter"class="form-control form-control-lg" />
+            	<div class="block-23 mb-10">
+                  <i class="fa-brands fa-github"></i>
+                  <i class="fa-brands fa-youtube"></i>
+                   <i class="fa-brands fa-twitter"></i>
+	
+	            </div>
+            </div>
           </div>
-            <i class="fa-brands fa-github"></i>
-            <i class="fa-brands fa-youtube"></i>
-            <i class="fa-brands fa-twitter"></i>
-        
-
-  
-    <p class=""> @ Copyright 2022Huddle. All rights reserved</p>
+        </div>
+      </div>
+      <div class="container-fluid px-0 py-4">
+      	<div class="container">
+		
+	            <p class= "text-end">
+	  Copyright <script>document.write(new Date().getFullYear());</script> Auth_Wiki All rights reserved
+	  </p>
+      	</div>
+      </div>
     </footer>
-</div>
-</div>
-</div>
-</div>
-
 </body>
 </html>
