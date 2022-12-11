b2faf6071bebe596b4694f2d4c29ec3e33788885
Chrisblurp <adeyemi504@gmail.com>
2022-12-11 02:58:28 +0100

    updated

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,36 +6,46 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- Bootstrap cdn -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 <!-- Font Awesome Cdn Link -->
 <script src="https://kit.fontawesome.com/ca85a7dd46.js" crossorigin="anonymous"></script>
 <!--Google font Link  -->
-
+<link href="/css/footer.css" rel="stylesheet">
 <title>Footer</title>
 </head>
 <body>
     <!-- The footer section -->
-    <footer>
-    <img src="{{URL('images/logo-icon-sm.png')}}" alt="Logo Pic>
-    <div class="">
-        <div class="">
-           
-            <p> Install</p>
-            <p>Documentation</p>
-            <p> Learn</p>
-            <p>Roadmap</p>
+    <div class=" container-fluid my-5">
+    <footer class="text-center text-lg-start">
+    <div class="container p-4">
+        <div class="row my-5">
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+        <div class="d-flex  mb-4 ">
+    <img src="{{URL('images/logo-icon-sm.png')}}">
+</div>
+    
+  
+        
+            
+            <ul list-unstyled d-flex flex-row justify-content-center>
+                <li> <a href="">Install</a> </li>
+                <li><a href="">Documentation</a></li>
+                <li><a href="">Learn</a></li>
+                <li><a href="">Roadmap</a></li>
+            </ul>
 
            
-        </div>
-        <div class="">
-            <ul>
+        
+        
+            <ul list-unstyled d-flex flex-row justify-content-center>
                 <li> <a href="">About Us</a> </li>
                 <li><a href="">Community</a></li>
                 <li><a href="">User surveys</a></li>
                 <li><a href="">contribute</a></li>
                 <li><a href="">Code of conduct</a></li>
             </ul>
-        </div>
-        <div>
+        
             <ul class="">
                 <li> <a href="">Get help</a></li>
                 <li><a href="">Terms of use</a></li>
@@ -43,19 +53,25 @@
                 <li><a href="">Press Kit</a></li>
                 <li><a href="">User surveys</a></li>
             </ul>
-        </div>
+       
 
-        <div class="">
+            <p> Sign up for Latest Auth_wiki news, resources and more </p>
+            <div class="form-outline form-white mb-4">
+            <input type="text" id="formControlLg" placeholder="Press enter"class="form-control form-control-lg" />
+
+          </div>
             <i class="fa-brands fa-github"></i>
             <i class="fa-brands fa-youtube"></i>
             <i class="fa-brands fa-twitter"></i>
-        </div>
+        
 
-    </div>
-    <p class=""> @ Copyright 2020 Huddle. All rights reserved</p>
-    </footer>
   
-<!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <p class=""> @ Copyright 2022Huddle. All rights reserved</p>
+    </footer>
+</div>
+</div>
+</div>
+</div>
+
 </body>
 </html>
