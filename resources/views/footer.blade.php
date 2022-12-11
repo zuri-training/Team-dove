a5d7a48d5a5ae1e8b0409b9690506fb3f29e46e9
Chrisblurp <adeyemi504@gmail.com>
2022-12-11 01:59:23 +0100

    update

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap cdn -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<!-- Font Awesome Cdn Link -->
+<script src="https://kit.fontawesome.com/ca85a7dd46.js" crossorigin="anonymous"></script>
+<!--Google font Link  -->
+
+<title>Footer</title>
+</head>
+<body>
+    <!-- The footer section -->
+    <footer>
+    <img src="{{URL('images/logo-icon-sm.png')}}" alt="Logo Pic>
+    <div class="">
+        <div class="">
+           
+            <p> Install</p>
+            <p>Documentation</p>
+            <p> Learn</p>
+            <p>Roadmap</p>
+
+           
+        </div>
+        <div class="">
+            <ul>
+                <li> <a href="">About Us</a> </li>
+                <li><a href="">Community</a></li>
+                <li><a href="">User surveys</a></li>
+                <li><a href="">contribute</a></li>
+                <li><a href="">Code of conduct</a></li>
+            </ul>
+        </div>
+        <div>
+            <ul class="">
+                <li> <a href="">Get help</a></li>
+                <li><a href="">Terms of use</a></li>
+                <li><a href=""> Privacy</a></li>
+                <li><a href="">Press Kit</a></li>
+                <li><a href="">User surveys</a></li>
+            </ul>
+        </div>
+
+        <div class="">
+            <i class="fa-brands fa-github"></i>
+            <i class="fa-brands fa-youtube"></i>
+            <i class="fa-brands fa-twitter"></i>
+        </div>
+
+    </div>
+    <p class=""> @ Copyright 2020 Huddle. All rights reserved</p>
+    </footer>
+  
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-
+use App\Http\Controllers\footer;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function(){
+    return view('footer');
 });
