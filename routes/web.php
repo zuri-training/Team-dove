c9ecb92d906c6038a9e7511d737c9976160e5d55
Chrisblurp <adeyemi504@gmail.com>
2022-12-11 18:38:02 +0100

    header added

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::get('/', function(){
+//     return view('footer',);
+// });
+
 Route::get('/', function(){
-    return view('footer');
+    return view('header');
 });
