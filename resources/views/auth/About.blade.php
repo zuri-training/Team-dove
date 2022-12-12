6b9805063b694f95739095962e75e39e231bed1e
Dejiadesina <apple@macs-MacBook-Pro.local>
2022-12-12 22:53:23 +0100

    initial commit about-us page

---
+++ resources/views/auth/About.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="about.css" />
+</head>
+<body>
+@include('layout.topbar')
+    <div class="about">
+        <h1 class='abt-heading'> About Auth_wiki </h1>
+        <p>Auth_wiki is an open source platform, built and maintained by developers like you, to make the work easier for everyone.
+         Build and deploy your projects faster, with our suite of user authentication libraries for major programming languages and frameworks.
+         Auth_Wiki was built on Laravel, an open-source PHP framework for Artisans. </p>
+    </div>
+    <div class="why">
+        <div> 
+        <img src="Images/intro.png" alt="img" />
+        </div>
+        <div class='why2'>
+            <h2>Why Our Auth-Wiki Library?</h2>
+            <p class="multi">Auth-Wiki Multi-Factor authentication is a layered approach to securing data and applications where a system reqquires a user to 
+                present a combination of two or more credentials to verify a user’s identity for login. to increase because even if one credential
+                 becomes compromised, unauthorised user’s will be unable to meet the second authentication requirement.....View More</p>
+        </div>
+       
+    </div>
+    <div class='abt-footer'>
+        <p class='heading'>Meet our team</p>
+        <div class='team-images'>
+            <div class='img-text'>
+            <img src="Images/otusanya.png" alt="team-lead" />
+            <p>Otunsanya Iyabo<p>
+            <p class='role'>Team lead<p>
+           
+            </div>
+            <div class='img-text'>
+            <img src="Images/obinna-akaolisa.png" alt="dev-lead" />
+            <p>obinna Akaolisa<p>
+            <p class='role'>Developer Team lead<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/babatunde-olanrewaju.png" alt="Ass-lead" />
+            <p>Babatunde Olanrewaju<p>
+            <p class='role'>Ass. Team lead<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/christopher-adeyemi.png" alt="Ass.Dev-lead" />
+            <p>Christopher Adeyemi<p>
+            <p class='role'>Ass. Developer Team lead<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/godwin-uchechukwu.png" alt="team-lead" />
+            <p>Godwin Uchechukwu<p>
+            <p class='role'>Product Designer Team lead<p>
+            </div>
+        </div>
+        <div class='team-images'>
+            <div class='img-text'>
+            <img src="Images/aisha-sanusi.png" alt="team-lead" />
+            <p>Aisha Sanusi<p>
+            <p class='role'>Product Designer Ass. Team lead<p>
+           
+            </div>
+            <div class='img-text'>
+            <img src="Images/jemine-okorodudu.png" alt="team-lead" />
+            <p>Jemine Okorodudu<p>
+            <p class='role'>Product Designer<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/dorcas-samuel.png" alt="team-lead" />
+            <p>Dorcas Samuel<p>
+            <p class='role'>Frontend + PHP<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/confidence-asor.png" alt="team-lead" />
+            <p>Confidence Asor<p>
+            <p class='role'>Product Designer<p>
+            </div>
+            <div class='img-text'>
+            <img src="Images/otusanya.png" alt="team-lead" />
+            <p>Otunsanya Iyabo<p>
+            <p class='role'>Product Designer<p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
