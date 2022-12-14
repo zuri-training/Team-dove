152cac48918cf772c2508461a9ca82585a34e822
obinnaakaolisa <akaolisaobinna@gmail.com>
2022-12-14 05:40:52 +0100

    Added link to favicon

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -10,6 +10,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!--Import Custom CSS-->
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <!--Import favicon-->
+    <link rel="shortcut icon" href="{{ asset('images/favicon.png') }}" type="image/x-icon">
     <title>About Auth_Wiki - Team Dove</title>
     <link rel="stylesheet" href="about.css" />
 </head>
