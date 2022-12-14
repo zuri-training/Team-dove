a96177e506ce6807da1984650e08a883062df666
Obinna Akaolisa <39627400+obinnaakaolisa@users.noreply.github.com>
2022-12-14 15:11:49 +0100

    Reset password page design

---
+++ app/Http/Controllers/ResetPasswordController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ResetPasswordController extends Controller
+{
+   //
+   public function index()
+   {
+       return view('auth.resetPassword');
+   }
+
+   public function store(Request $request)
+   {
+       
+   }
+}
